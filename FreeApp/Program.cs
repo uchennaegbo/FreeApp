96ffc89005a550df385668fda709e11c8e59a54c
Uchenna Egbo <egbouchenna001@gmail.com>
2020-02-07 12:15:35 +0100

    Add project files.

---
+++ FreeApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FreeApp
+{
+    class Program
+    {
+        private const char Value = '!';
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!".EndsWith(Value));
+        }
+    }
+}
